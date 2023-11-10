ed1c1e0b2903e935acdcaf78c7f44281c2bd812f
Batug Kocak <batugkocak@gmail.com>
2023-11-10 11:22:42 +0300

    bugfix

--- DataAccess/Concrete/EntityFramework/EfVehicleOnTaskDal.cs
+++ DataAccess/Concrete/EntityFramework/EfVehicleOnTaskDal.cs
@@ -30,7 +30,6 @@ public class EfVehicleOnTaskDal: EfEntityRepositoryBase<VehicleOnTask, VehicleDe
             join vehicle in context.Vehicles on v.VehicleId equals vehicle.Id 
             where v.IsDeleted != true
             where v.IsFinished == true
-            orderby v.GivenDate descending 
             select new VehicleOnTaskDetailDto{
                 VehicleOnTaskId = v.Id,
                 VehiclePlate = vehicle.Plate,
@@ -42,7 +41,43 @@ public class EfVehicleOnTaskDal: EfEntityRepositoryBase<VehicleOnTask, VehicleDe
                 Address = v.Address,
                 GivenDate = v.GivenDate,
                 ReturnDate = v.ReturnDate
-            }).ToList();
+            }).OrderByDescending(vot => vot.GivenDate).ToList();
+        return result;
+    }
+    public PagingResponse<VehicleOnTaskForTableDto> GetVehicleOnTaskForTableFinished(VotFilterRequest filterRequest)
+    {
+        using VehicleDeliveryContext context = new VehicleDeliveryContext();
+        var vehiclesOnTask = context.VehiclesOnTask.AsQueryable();
+        if (filterRequest.DateType == "returnDate")
+        {
+            vehiclesOnTask = vehiclesOnTask.Where(v =>
+                filterRequest.FirstDate <= v.ReturnDate && filterRequest.LastDate >= v.ReturnDate);
+        }
+        else //default 
+        {
+            vehiclesOnTask = vehiclesOnTask.Where(v =>
+                filterRequest.FirstDate <= v.GivenDate && filterRequest.LastDate >= v.GivenDate);
+        } 
+        var result = (from vot in vehiclesOnTask
+            join vehicle in context.Vehicles on vot.VehicleId equals vehicle.Id
+            join d in context.Departments on vot.DepartmentId equals d.Id
+            join department in context.Departments on vot.DepartmentId equals department.Id
+            where vot.IsDeleted != true
+            where vot.IsFinished == true
+            select new VehicleOnTaskForTableDto()
+            {
+                Id = vot.Id,
+                VehicleId = vot.VehicleId,
+                VehiclePlate = vehicle.Plate,
+                DepartmentName = department.Name,
+                TaskDefinition = vot.TaskDefinition,
+                GivenDate = vot.GivenDate,
+                ReturnDate = vot.ReturnDate,
+            }).OrderByDescending(vot => vot.GivenDate).ToPaginate(new ()
+        {
+            PageSize = filterRequest.Size,
+            PageIndex = filterRequest.Page
+        });
         return result;
     }
     
@@ -94,44 +129,7 @@ public class EfVehicleOnTaskDal: EfEntityRepositoryBase<VehicleOnTask, VehicleDe
     }
 
 
-    public PagingResponse<VehicleOnTaskForTableDto> GetVehicleOnTaskForTableFinished(VotFilterRequest filterRequest)
-    {
-        using VehicleDeliveryContext context = new VehicleDeliveryContext();
-        var vehiclesOnTask = context.VehiclesOnTask.AsQueryable();
-        if (filterRequest.DateType == "returnDate")
-        {
-            vehiclesOnTask = vehiclesOnTask.Where(v =>
-                filterRequest.FirstDate <= v.ReturnDate && filterRequest.LastDate >= v.ReturnDate);
-        }
-        else //default 
-        {
-            vehiclesOnTask = vehiclesOnTask.Where(v =>
-                filterRequest.FirstDate <= v.GivenDate && filterRequest.LastDate >= v.GivenDate);
-        } 
-        var result = (from vot in vehiclesOnTask
-            join vehicle in context.Vehicles on vot.VehicleId equals vehicle.Id
-            join d in context.Departments on vot.DepartmentId equals d.Id
-            join department in context.Departments on vot.DepartmentId equals department.Id
-            where vot.IsDeleted != true
-            where vot.IsFinished == true
-            where filterRequest.FirstDate <= vot.GivenDate
-            where filterRequest.LastDate >= vot.GivenDate
-            select new VehicleOnTaskForTableDto()
-            {
-                Id = vot.Id,
-                VehicleId = vot.VehicleId,
-                VehiclePlate = vehicle.Plate,
-                DepartmentName = department.Name,
-                TaskDefinition = vot.TaskDefinition,
-                GivenDate = vot.GivenDate,
-                ReturnDate = vot.ReturnDate,
-            }).OrderByDescending(vot => vot.ReturnDate).ToPaginate(new ()
-        {
-            PageSize = filterRequest.Size,
-            PageIndex = filterRequest.Page
-        });
-        return result;
-    }
+    
 }
 
 // .OrderByDescending(vot => vot.Id)
